a189f0b56fc0563de0b343c6c7d8b9627d8ea9ae
CodeCrafter39 <CodeCrafter39@yandex.ru>
2024-07-30 20:13:14 +0300

    задание 4

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended
+];
